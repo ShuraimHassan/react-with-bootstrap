20b6a6568571efae663a564e6a62583a7f3d05d7
ShuraimHassan <Shuraimhassan94@gmail.com>
2021-06-14 02:57:25 +0500

    John sigma project files added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react'
 // 1) Book Store Component with js seperete data file of an array of books
 // import PublishBookStore from './shared/PublishBookStore';
-import HandleNameComponent from './Components/HandleNameComponent';
+// import HandleNameComponent from './Components/HandleNameComponent';
+import React from 'react'
+import Array from './tutorial/1-useState/final/3-useState-array'
 
 
 export default function App() {
@@ -9,8 +10,7 @@ export default function App() {
 
   return (
     <>
-    App.js
-      <HandleNameComponent />
+    <Array/>
 
     </>
   )
--- src/tutorial/1-useState/final/3-useState-array.js
+++ src/tutorial/1-useState/final/3-useState-array.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useState } from 'react';
+// import { data } from '../../../data';
+import { Profile } from '../../../Js/name';
+
 const UseStateArray = () => {
-  const [people, setPeople] = React.useState(data);
+  const [people, setPeople] = useState(Profile);
 
   const removeItem = (id) => {
     let newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
   };
+
+  const clearList = (id) => setPeople([]);
+  const recoverData = (id) => setPeople(Profile);
+
+
   return (
     <>
-      {people.map((person) => {
-        const { id, name } = person;
+      {people.map(({ id, name, age, proffesion, ...props }) => {
+
         return (
-          <div key={id} className='item'>
-            <h4>{name}</h4>
-            <button onClick={() => removeItem(id)}>remove</button>
+          <div key={id}  className='container card card-body btn-sm '>
+            <h4>{name} is old of {age} and his/her working as {proffesion}</h4>
+            <h5 >Favourite Quote-<q>{props.hobbies.favQuotes}</q></h5>
+            <button className='btn-sm btn' onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button className='btn' onClick={() => setPeople([])}>
-        clear items
+      <button className='btn' onClick={clearList}>
+        Remove All
+      </button>
+      <button className='btn' onClick={recoverData}>
+        Recover Data
       </button>
     </>
   );
